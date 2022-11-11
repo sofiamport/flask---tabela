3a8d3817bff44a6e0b0c614e33325faf012490da
sof <83407605+sofiamport@users.noreply.github.com>
2022-11-11 15:41:59 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect
+from module.database import Database
+
+app= Flask(__name__)
+
+
+app = Flask(__name__)
+app.secret_key = "mys3cr3tk3y"
+db = Database()
+
+@app.route('/')
+def index():
+    data = db.read(None)
+    return render_template('index.html', data = data)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addphone', methods = ['POST', 'GET'])
+def addphone():
+    if request.method == 'POST' and request.form['save']:
+        if db.insert(request.form):
+            flash("A new phone number has been added")
+        else:
+            flash("A new phone number can not be added")
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/update/<int:id>')
+def update(id):
+    data = db.read(id);
+
+    if len(data) == 0:
+        return redirect(url_for('index'))
+    else:
+        session['update'] = id
+        return render_template('update.html', data = data)
+
+@app.route('/updatephone', methods = ['POST'])
+def updatephone():
+    if request.method == 'POST' and request.form['update']:
+
+        if db.update(session['update'], request.form):
+            flash('A phone number has been updated')
+
+        else:
+            flash('A phone number can not be updated')
+
+        session.pop('update', None)
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>/')
+def delete(id):
+    data = db.read(id);
+
+    if len(data) == 0:
+        return redirect(url_for('index'))
+    else:
+        session['delete'] = id
+        return render_template('delete.html', data = data)
+
+@app.route('/deletephone', methods = ['POST'])
+def deletephone():
+    if request.method == 'POST' and request.form['delete']:
+
+        if db.delete(session['delete']):
+            flash('A phone number has been deleted')
+
+        else:
+            flash('A phone number can not be deleted')
+
+        session.pop('delete', None)
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+app.run(port=8081)
